08ae34246980bf338554a1572d941f926c283c36
Navedita Katoch <katochna@sheridancollege.ca>
2024-01-26 16:43:52 -0500

    Third Commit

---
+++ src/partTimeStudent.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class partTimeStudent extends Student {
+    private int NumOfCourses;
+
+    public partTimeStudent(int NumOfCourses) {
+        this.NumOfCourses = NumOfCourses;
+    }
+
+    public int getNumOfCourses() {
+        return NumOfCourses;
+    }
+
+    public void setNumOfCourses(int NumOfCourses) {
+        this.NumOfCourses = NumOfCourses;
+    }
+    
+    
+
+}
